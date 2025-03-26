a70ccd6f7d6a75c71e8c08a479902ba2ef843e9d
WayneSu <sekzerd@outlook.com>
2025-03-26 11:48:42 +0800

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse,FileResponse
+from fastapi.responses import FileResponse
 
 
 WEB_PATH = "../web/"
 app = FastAPI()
+
 
 # handle root to index.html
 @app.get("/")
 async def root():
     return FileResponse(WEB_PATH + "index.html")
 
+
 # configure web path
 app.mount("/", StaticFiles(directory=WEB_PATH), name="static")
 
-import uvicorn
-
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
