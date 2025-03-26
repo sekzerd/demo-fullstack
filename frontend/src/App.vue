610986bceae24b04201f09f6b8d7ab9964e91983
WayneSu <sekzerd@outlook.com>
2025-03-26 11:42:32 +0800

    init repo

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex justify-center gap-2">
+    <button class="btn btn-error text-white mt-2 border-green-300 hover:bg-white hover:text-black">click</button>
+  </div>
+  <div class="flex justify-center gap-2 mt-10">
+    <div class="flex flex-col gap-2 items-center">
+      <div class="bg-red-100 w-10 h-10 rounded-full"></div>
+      <div class="bg-red-200 w-1 h-20"></div>
+      <div class="bg-red-300 w-10 h-10"></div>
+      <div class="bg-red-400 w-10 h-10"></div>
+      <div class="bg-red-500 w-10 h-10"></div>
+      <div class="bg-red-600 w-10 h-10"></div>
+      <div class="bg-red-700 w-10 h-10"></div>
+      <div class="bg-red-800 w-10 h-10"></div>
+      <div class="bg-red-900 w-10 h-10"></div>
+    </div>
+    <div class="flex flex-col gap-2 items-center">
+      <div class="bg-green-100 w-10 h-10"></div>
+      <div class="bg-green-200 w-10 h-10"></div>
+      <div class="bg-green-300 w-10 h-10"></div>
+      <div class="bg-green-400 w-10 h-10"></div>
+      <div class="bg-green-500 w-10 h-10"></div>
+      <div class="bg-green-600 w-10 h-10"></div>
+      <div class="bg-green-700 w-10 h-10"></div>
+      <div class="bg-green-800 w-10 h-10"></div>
+      <div class="bg-green-900 w-10 h-10"></div>
+    </div>
+    <div class="flex flex-col gap-2 items-center">
+      <div class="bg-blue-100 w-10 h-10"></div>
+      <div class="bg-blue-200 w-10 h-10"></div>
+      <div class="bg-blue-300 w-10 h-10"></div>
+      <div class="bg-blue-400 w-10 h-10"></div>
+      <div class="bg-blue-500 w-10 h-10"></div>
+      <div class="bg-blue-600 w-10 h-10"></div>
+      <div class="bg-blue-700 w-10 h-10"></div>
+      <div class="bg-blue-800 w-10 h-10"></div>
+      <div class="bg-blue-900 w-10 h-10"></div>
+    </div>
+  </div>
+</template>
